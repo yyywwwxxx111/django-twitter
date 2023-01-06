99eb04df5c8d60a70ac3b7c131b98a79371af099
yyywwwxxx111 <343083317@qq.com>
2023-01-05 19:20:03 -0500

    Add newsfeeds api and testcases

--- twitter/urls.py
+++ twitter/urls.py
@@ -19,6 +19,8 @@ from rest_framework import routers
 from accounts.api.views import UserViewSet, AccountViewSet
 from tweets.api.views import TweetViewSet
 from friendships.api.views import FriendshipViewSet
+from newsfeeds.api.views import NewsFeedViewSet
+
 
 import debug_toolbar
 
@@ -28,6 +30,8 @@ router.register(r'api/users', UserViewSet)
 router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
+router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
+
 
 
 
