99539e187c8744c1cf59a4347588e2a75c73d7ea
yyywwwxxx111 <343083317@qq.com>
2023-01-11 19:39:13 -0500

    Add Friendship pagination

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,7 +1,6 @@
-from django.contrib.auth.models import User
-
 from accounts.api.serializers import UserSerializerForFriendship
 from friendships.models import Friendship
+from friendships.services import FriendshipService
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -19,17 +18,6 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
             raise ValidationError({
                 'message': 'from_user_id and to_user_id should be different'
             })
-        # if Friendship.objects.filter(
-        #     from_user_id=attrs['from_user_id'],
-        #     to_user_id=attrs['to_user_id'],
-        # ).exists():
-        #     raise ValidationError({
-        #         'message': 'You has already followed this user.'
-        #     })
-        if not User.objects.filter(id=attrs['to_user_id']).exists():
-            raise ValidationError({
-                'message': 'You can not follow a non-exists user.'
-            })
         return attrs
 
     def create(self, validated_data):
@@ -45,17 +33,35 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
 # 即 model_instance.xxx 来获得数据
 # https://www.django-rest-framework.org/api-guide/serializers/#specifying-fields-explicitly
 class FollowerSerializer(serializers.ModelSerializer):
-    # source: 用user代替from_user
     user = UserSerializerForFriendship(source='from_user')
+    created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
+        # 我们将在后序的课程中解决这个问题
+        return FriendshipService.has_followed(self.context['request'].user, obj.from_user)
 
 
 class FollowingSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='to_user')
+    created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
+        # 我们将在后序的课程中解决这个问题
+        return FriendshipService.has_followed(self.context['request'].user, obj.to_user)
--- friendships/api/views.py
+++ friendships/api/views.py
@@ -1,47 +1,43 @@
+from django.contrib.auth.models import User
+from friendships.api.paginations import FriendshipPagination
+from friendships.api.serializers import (
+    FollowerSerializer,
+    FollowingSerializer,
+    FriendshipSerializerForCreate,
+)
+from friendships.models import Friendship
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from friendships.models import Friendship
-from friendships.api.serializers import (
-    FollowingSerializer,
-    FollowerSerializer,
-    FriendshipSerializerForCreate,
-)
-from django.contrib.auth.models import User
 
 
-class FriendshipViewSet(viewsets.GenericViewSet, viewsets.mixins.ListModelMixin):
+class FriendshipViewSet(viewsets.GenericViewSet):
     # 我们希望 POST /api/friendship/1/follow 是去 follow user_id=1 的用户
     # 因此这里 queryset 需要是 User.objects.all()
     # 如果是 Friendship.objects.all 的话就会出现 404 Not Found
     # 因为 detail=True 的 actions 会默认先去调用 get_object() 也就是
     # queryset.filter(pk=1) 查询一下这个 object 在不在
-    serializer_class = FriendshipSerializerForCreate
     queryset = User.objects.all()
+    # 一般来说，不同的 views 所需要的 pagination 规则肯定是不同的，因此一般都需要自定义
+    pagination_class = FriendshipPagination
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followers(self, request, pk):
         friendships = Friendship.objects.filter(to_user_id=pk).order_by('-created_at')
-        serializer = FollowerSerializer(friendships, many=True)
-        return Response(
-            {'followers': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowerSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         friendships = Friendship.objects.filter(from_user_id=pk).order_by('-created_at')
-        serializer = FollowingSerializer(friendships, many=True)
-        return Response(
-            {'followings': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowingSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
-        # check if user with id=pk exists
-        self.get_object()  # raise 404
         # 特殊判断重复 follow 的情况（比如前端猛点好多少次 follow)
         # 静默处理，不报错，因为这类重复操作因为网络延迟的原因会比较多，没必要当做错误处理
         if Friendship.objects.filter(from_user=request.user, to_user=pk).exists():
@@ -58,9 +54,8 @@ class FriendshipViewSet(viewsets.GenericViewSet, viewsets.mixins.ListModelMixin)
                 'success': False,
                 'errors': serializer.errors,
             }, status=status.HTTP_400_BAD_REQUEST)
-        instance = serializer.save()
-        return Response(FollowingSerializer(instance).data,
-                        status=status.HTTP_201_CREATED)
+        serializer.save()
+        return Response({'success': True}, status=status.HTTP_201_CREATED)
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def unfollow(self, request, pk):
@@ -82,6 +77,3 @@ class FriendshipViewSet(viewsets.GenericViewSet, viewsets.mixins.ListModelMixin)
             to_user=pk,
         ).delete()
         return Response({'success': True, 'deleted': deleted})
-
-    def list(self, request, *args, **kwargs):
-        return Response({'msg': 'homepage'})
